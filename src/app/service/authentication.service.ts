89133d7b97cf41b4fcc940a4254e8ad598aa178d
Bianca <manea.bianca9@yahoo.ro>
2025-06-25 18:59:36 +0300

    Creating authentication service

---
+++ src/app/service/authentication.service.ts
@@ -0,0 +1,76 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient, HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { User } from '../model/user';
+import { JwtHelperService } from '@auth0/angular-jwt';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+
+  private host = environment.apiUrl;
+  private token: string;
+  private loggedInUsername: string;
+  private jwtHelper = new JwtHelperService();
+
+  constructor(private http: HttpClient) { }
+
+  // can either return a response or an error
+  public login(user: User): Observable<HttpResponse<any> | HttpErrorResponse> {
+    return this.http.post<HttpResponse<any> | HttpErrorResponse>(`${this.host}/user/login`, user, {observe: 'response'});  // we want the whole HTTP response back
+  }
+
+  public register(user: User): Observable<User | HttpErrorResponse> {
+    return this.http.post<User | HttpErrorResponse>(`${this.host}/user/register`, user );
+  }
+
+  public logout(): void {
+    //clear the local storage
+    this.token = null;
+    this.loggedInUsername = null;
+    //remove the items from local storage
+    localStorage.removeItem('user');
+    localStorage.removeItem('token');
+    localStorage.removeItem('users');
+  }
+
+  public saveToken(token: string): void {
+    this.token = token;
+    localStorage.setItem('token', token); //key-value pair in local storage
+  }
+
+  public addUserToLocalCache(user: User): void {
+    localStorage.setItem('user', JSON.stringify(user));
+  }
+
+  // get the user from local storage and parse it to a User object
+  public getUserFromLocalCache(): User {
+    return JSON.parse(localStorage.getItem('user'));
+  }
+
+  public loadToken(): void {
+    this.token = localStorage.getItem('token');
+  }
+
+  public getToken(): string {
+    return this.token;
+  }
+
+  public isLoggedIn(): boolean {
+    this.loadToken();
+    if(this.token != null  && this.token !== ''){
+      if(this.jwtHelper.decodeToken(this.token).sub != null || ''){
+        if(!this.jwtHelper.isTokenExpired(this.token)){
+          this.loggedInUsername = this.jwtHelper.decodeToken(this.token).sub;
+          return true;
+        }
+      }
+    }else{
+      this.logout();
+      return false;
+    }
+  }
+
+}
