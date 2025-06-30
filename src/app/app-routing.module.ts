1020bca6ff254dbe790d0906f78af4dda8c33d39
Bianca <manea.bianca9@yahoo.ro>
2025-06-30 19:08:50 +0300

    Generating components and configuring routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { UserComponent } from './user/user.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  // order matters, so the first one that matches will be used
+  // if you put the empty path first, it will always match and redirect to login
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'user/management', component: UserComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
